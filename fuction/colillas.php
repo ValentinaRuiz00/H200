0035ffe24a1449d62866d0d3e7ea79f24b595595
AmayaXitado <amaya78951@gmail.com>
2023-11-22 09:59:01 -0500

    Update 9

--- fuction/colillas.php
+++ fuction/colillas.php
@@ -1,103 +1,106 @@
 <?php
-require('../fpdf/fpdf.php'); // Ajusta la ruta según la estructura de tu proyecto
 
-class PDF extends FPDF
-{
-    // Encabezado de página
-    function Header()
-    {
-        $this->SetFont('Arial','B',12);
-        $this->Cell(0,10,'Este es el encabezado de la tabla',0,1,'C');
+
+// somewhere early in your project's loading, require the Composer autoloader
+// see: http://getcomposer.org/doc/00-intro.md
+require '../vendor/autoload.php';
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
+
+// instantiate and use the dompdf class
+$dompdf = new Dompdf();
+$dompdf->loadHtml('DOCUMENTO HECHO CON DOM PDF
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+    body {
+      font-family: Arial, sans-serif;
     }
 
-    // Pie de página
-    function Footer()
-    {
-        $this->SetFont('Arial','B',12);
-        $this->Cell(0,10,'Este es el encabezado de la tabla',0,1,'C');
+    table {
+      width: 100%;
+      border-collapse: collapse;
+      margin-bottom: 20px;
     }
-}
 
-$pdf = new PDF();
-$pdf->AddPage();
-$pdf->SetFont('Arial','',10);
+    th, td {
+      border: 1px solid #ddd;
+      padding: 8px;
+      text-align: left;
+    }
 
+    th {
+      background-color: #f2f2f2;
+    }
 
-$pdf->Cell(40,10,'Celda1 ',1);
-$pdf->Cell(40,10,'Celda2 ',1);
-$pdf->Cell(40,10,'Celda3 ',1);
-$pdf->Ln();
-$pdf->Cell(40,10,'Celda1 ',1);
-$pdf->Cell(40,10,'Celda2 ',1);
-$pdf->Cell(40,10,'Celda3 ',1);
-$pdf->Ln();
-$pdf->Cell(40,10,'Celda1 ',1);
-$pdf->Cell(40,10,'Celda2 ',1);
-$pdf->Cell(40,10,'Celda3 ',1);
-$pdf->Ln();
-$pdf->Cell(40,10,'Celda1 ',1);
-$pdf->Cell(40,10,'Celda2 ',1);
-$pdf->Cell(40,10,'Celda3 ',1);
-$pdf->Ln();
-$pdf->Cell(40,10,'Celda1 ',1);
-$pdf->Cell(40,10,'Celda2 ',1);
-$pdf->Cell(40,10,'Celda3 ',1);
-$pdf->Ln();
-$pdf->Cell(40,10,'Celda1 ',1);
-$pdf->Cell(40,10,'Celda2 ',1);
-$pdf->Cell(40,10,'Celda3 ',1);
-$pdf->Ln();
-$pdf->Cell(40,10,'Celda1 ',1);
-$pdf->Cell(40,10,'Celda2 ',1);
-$pdf->Cell(40,10,'Celda3 ',1);
-$pdf->Ln();
-$pdf->Cell(40,10,'Celda1 ',1);
-$pdf->Cell(40,10,'Celda2 ',1);
-$pdf->Cell(40,10,'Celda3 ',1);
-$pdf->Ln();
-$pdf->Cell(40,10,'Celda1 ',1);
-$pdf->Cell(40,10,'Celda2 ',1);
-$pdf->Cell(40,10,'Celda3 ',1);
-$pdf->Ln();
-$pdf->Cell(40,10,'Celda1 ',1);
-$pdf->Cell(40,10,'Celda2 ',1);
-$pdf->Cell(40,10,'Celda3 ',1);
-$pdf->Ln();
-$pdf->Cell(40,10,'Celda1 ',1);
-$pdf->Cell(40,10,'Celda2 ',1);
-$pdf->Cell(40,10,'Celda3 ',1);
-$pdf->Ln();
-$pdf->Cell(40,10,'Celda1 ',1);
-$pdf->Cell(40,10,'Celda2 ',1);
-$pdf->Cell(40,10,'Celda3 ',1);
-$pdf->Ln();
-$pdf->Cell(40,10,'Celda1 ',1);
-$pdf->Cell(40,10,'Celda2 ',1);
-$pdf->Cell(40,10,'Celda3 ',1);
-$pdf->Ln();
-$pdf->Cell(40,10,'Celda1 ',1);
-$pdf->Cell(40,10,'Celda2 ',1);
-$pdf->Cell(40,10,'Celda3 ',1);
-$pdf->Ln();
-$pdf->Cell(40,10,'Celda1 ',1);
-$pdf->Cell(40,10,'Celda2 ',1);
-$pdf->Cell(40,10,'Celda3 ',1);
-$pdf->Ln();
-$pdf->Cell(40,10,'Celda1 ',1);
-$pdf->Cell(40,10,'Celda2 ',1);
-$pdf->Cell(40,10,'Celda3 ',1);
-$pdf->Ln();
-$pdf->Cell(40,10,'Celda1 ',1);
-$pdf->Cell(40,10,'Celda2 ',1);
-$pdf->Cell(40,10,'Celda3 ',1);
-$pdf->Ln();
-$pdf->Cell(40,10,'Celda1 ',1);
-$pdf->Cell(40,10,'Celda2 ',1);
-$pdf->Cell(40,10,'Celda3 ',1);
-$pdf->Ln();
+    h2 {
+      color: #333;
+    }
 
+    /* Nueva clase para alinear a la derecha */
+    .text-right {
+      text-align: right;
+    }
+  </style>
+  <title>Detalle de Sueldo</title>
+</head>
+<body>
 
+  <h2>Detalle de Sueldo</h2>
 
+  <table>
+    <thead>
+      <tr>
+        <th>Concepto</th>
+        <th>   Debe </th>
+        <th>   Haber </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Salario Base</td>
+        <td class="text-right">$2,000.00</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Antigüedad</td>
+        <td class="text-right">$100.00</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td><strong>Total sin Retenciones</strong></td>
+        <td class="text-right"><strong>$2,100.00</strong></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Retenciones</td>
+        <td></td>
+        <td class="text-right">-$50.00</td>
+      </tr>
+      <tr>
+        <td><strong>Total a Pagar</strong></td>
+        <td></td>
+        <td class="text-right"><strong>$2,050.00</strong></td>
+      </tr>
+    </tbody>
+  </table>
 
-$pdf->Output();
+</body>
+</html>
+
+');
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'frame');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+$dompdf->stream();
+
 ?>
